6660cdaa90342c1f41f0fe650ac1c274531c7ffc
taotao <taotao@mianchao-inc.com>
2020-06-01 14:40:41 +0800

    Updates

---
+++ precache-manifest.f034799b82d89ca1c7fd695f3f675906.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1938af7edd02532aa9aa3297ea939ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fc9922296fcfb60e785",
+    "url": "./static/css/main.52369e8b.chunk.css"
+  },
+  {
+    "revision": "9161d614fda3b72d0141",
+    "url": "./static/js/2.c3e9ab8c.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "./static/js/2.c3e9ab8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc9922296fcfb60e785",
+    "url": "./static/js/main.9698fe93.chunk.js"
+  },
+  {
+    "revision": "fe492bec4e044aabfe13",
+    "url": "./static/js/runtime-main.4f92633c.js"
+  },
+  {
+    "revision": "4ad07e23aa6dcea1cb54dbfc396681e1",
+    "url": "./static/media/bg.4ad07e23.png"
+  },
+  {
+    "revision": "c268bab5a96dceae66414ca06d6bf7ec",
+    "url": "./static/media/jt.c268bab5.png"
+  },
+  {
+    "revision": "0dea0f9739d68fcff939ba51d73a1e53",
+    "url": "./static/media/lbx.0dea0f97.png"
+  },
+  {
+    "revision": "bab2b509158ad04982f4527a1f3445fe",
+    "url": "./static/media/map.bab2b509.png"
+  }
+]);
